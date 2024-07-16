b6f50b6b1d3c6f0f52e446400dc7c3ced4a679d4
krzysiek <krzysztof.muhleisen@gmail.com>
2024-07-16 18:17:54 +0200

    buffer extracted to a separate file

--- include/ferrugo/fmt/format.hpp
+++ include/ferrugo/fmt/format.hpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <ferrugo/core/overloaded.hpp>
 #include <ferrugo/core/type_traits.hpp>
+#include <ferrugo/fmt/buffer.hpp>
 #include <functional>
 #include <iostream>
 #include <memory>
@@ -36,95 +37,6 @@ public:
 private:
     std::string_view m_specifier;
 };
-
-template <class T>
-struct basic_buffer
-{
-    using grow_function_type = std::size_t (*)(std::size_t);
-    std::size_t m_size;
-    std::size_t m_capacity;
-
-    grow_function_type m_grow_fn;
-    std::unique_ptr<T[]> m_data;
-
-    explicit basic_buffer(std::size_t capacity, grow_function_type grow_fn)
-        : m_size{ 0 }
-        , m_capacity{ capacity }
-        , m_grow_fn{ grow_fn }
-        , m_data{}
-    {
-        m_data.reset(new T[m_capacity]);
-    }
-
-    explicit basic_buffer() : basic_buffer(64, [](std::size_t n) { return 2 * n; })
-    {
-    }
-
-    std::size_t size() const
-    {
-        return m_size;
-    }
-
-    const T* begin() const
-    {
-        return m_data.get();
-    }
-
-    const T* end() const
-    {
-        return begin() + size();
-    }
-
-    T* begin()
-    {
-        return m_data.get();
-    }
-
-    T* end()
-    {
-        return begin() + size();
-    }
-
-    void ensure_capacity(std::size_t required_capacity)
-    {
-        if (required_capacity <= m_capacity)
-        {
-            return;
-        }
-
-        std::size_t new_capacity = m_capacity;
-        while (new_capacity < required_capacity)
-        {
-            new_capacity = m_grow_fn(new_capacity);
-        }
-
-        std::unique_ptr<T[]> ptr(new T[new_capacity]);
-        std::copy(begin(), end(), ptr.get());
-        m_data = std::move(ptr);
-        m_capacity = new_capacity;
-    }
-
-    void append(const T* b, const T* e)
-    {
-        std::size_t s = std::distance(b, e);
-        ensure_capacity(size() + s);
-        std::copy(b, e, end());
-        m_size += s;
-    }
-
-    void append(const T* b, std::size_t n)
-    {
-        append(b, b + n);
-    }
-
-    void reset()
-    {
-        m_size = 0;
-    }
-};
-
-using buffer = basic_buffer<char>;
-
 class format_context
 {
 public:
